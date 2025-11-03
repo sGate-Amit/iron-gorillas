7098651eca2951f73fd1d602ed767ba1e0334966
sGate-Amit <105426175+sGate-Amit@users.noreply.github.com>
2025-11-03 16:19:45 +0530

    Update astro.config.mjs
    code updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-   adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  output: "static",
+  image: {
+    service: "compile", // handles the "sharp" warning safely
+  },
+  build: {
+    inlineStylesheets: "never", // reduces memory footprint
+  },
 });
