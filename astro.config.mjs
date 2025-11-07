170308cca6a0cf3f18ca1c16009a719661d896ec
sGate-Amit <105426175+sGate-Amit@users.noreply.github.com>
2025-11-07 13:49:19 +0530

    Update astro.config.mjs
    config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/static';
+import { defineConfig } from "astro/config";
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  adapter: staticAdapter(),
-});
+  adapter: cloudflare({
+     imageService: 'cloudflare'
+  }),
+})
