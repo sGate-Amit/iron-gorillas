363c2fbeba6d332f6b3cdcc89ec3f742ed54745a
sGate-Amit <105426175+sGate-Amit@users.noreply.github.com>
2025-12-05 19:46:06 +0530

    Update astro.config.mjs
    code update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://example.com",
-  output: "static",
+  output: "hybrid",
   integrations: [mdx(), sitemap()],
   build: {
     inlineStylesheets: "never"
