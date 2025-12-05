e9cff817e2ae3e7cb808a25b7d9bbe23675acb6a
sGate-Amit <105426175+sGate-Amit@users.noreply.github.com>
2025-12-05 19:30:06 +0530

    Update astro.config.mjs
    code updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import staticAdapter from "@astrojs/static";
 
 export default defineConfig({
   site: "https://example.com",
   output: "static",
   integrations: [mdx(), sitemap()],
-  adapter: staticAdapter(),
 });
