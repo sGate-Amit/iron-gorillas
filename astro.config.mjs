e3fcb4878c2d949e7364f4dd7057da6010765826
sGate-Amit <105426175+sGate-Amit@users.noreply.github.com>
2025-12-15 15:53:34 +0530

    Update astro.config.mjs
    config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: "https://example.com",
   output: "hybrid",
